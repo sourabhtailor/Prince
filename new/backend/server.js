9a38550bc6f65aeaccc82617e820ca7422514643
sourabhtailor <sourabhtailortailor@gmail.com>
2023-10-30 11:24:59 +0530

    last

--- new/backend/server.js
+++ new/backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: 'db',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,

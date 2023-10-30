aa72e877c9509a8664a0b9804b13b46814102863
sourabhtailor <sourabhtailortailor@gmail.com>
2023-10-30 13:21:47 +0530

    3-tier last database

--- new/backend/server.js
+++ new/backend/server.js
@@ -1,53 +1,67 @@
 const express = require('express');
-const mysql = require('mysql');
+const mariadb = require('mariadb');
 
 const app = express();
 const port = 3000;
 
-const db = mysql.createConnection({
-  host: 'db',
+const pool = mariadb.createPool({
+  host: 'new-db-1', // Update with the actual hostname or service name
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  connectionLimit: 5, // Adjust this value as needed
 });
 
-db.connect(err => {
-  if (err) {
+// Testing the database connection
+pool.getConnection()
+  .then(conn => {
+    console.log('Database connected');
+    conn.end(); // Release the connection
+  })
+  .catch(err => {
     console.error('Database connection error:', err);
-    return;
-  }
-  
-  console.log('Database connected');
+  });
 
-  app.use(express.json());
+app.use(express.json());
 
-  app.get('/api/data', (req, res) => {
-    const sql = 'SELECT * FROM data';
-    db.query(sql, (err, results) => {
-      if (err) {
-        console.error('Error fetching data:', err);
-        res.status(500).json({ error: 'Error fetching data' });
-        return;
-      }
-      res.json(results);
+app.get('/api/data', (req, res) => {
+  pool.getConnection()
+    .then(conn => {
+      conn.query('SELECT * FROM data')
+        .then((rows) => {
+          conn.end(); // Release the connection
+          res.json(rows);
+        })
+        .catch((err) => {
+          conn.end(); // Release the connection
+          console.error('Error fetching data:', err);
+          res.status(500).json({ error: 'Error fetching data' });
+        });
+    })
+    .catch((err) => {
+      console.error('Database connection error:', err);
+      res.status(500).json({ error: 'Error connecting to the database' });
     });
-  });
+});
 
-  app.post('/api/data', (req, res) => {
-    const { value } = req.body;
-    const sql = 'INSERT INTO data (value) VALUES (?)';
-    db.query(sql, [value], (err, result) => {
-      if (err) {
-        console.error('Error adding data:', err);
-        res.status(500).json({ error: 'Error adding data' });
-        return;
-      }
-      res.json({ message: 'Data added successfully' });
+app.post('/api/data', (req, res) => {
+  const { value } = req.body;
+  pool.getConnection()
+    .then(conn => {
+      conn.query('INSERT INTO data (value) VALUES (?)', [value])
+        .then(() => {
+          conn.end(); // Release the connection
+          res.json({ message: 'Data added successfully' });
+        })
+        .catch((err) => {
+          conn.end(); // Release the connection
+          console.error('Error adding data:', err);
+          res.status(500).json({ error: 'Error adding data' });
+        });
     });
-  });
+});
 
-  app.listen(port, () => {
-    console.log(`Backend server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}`);
 });
 
